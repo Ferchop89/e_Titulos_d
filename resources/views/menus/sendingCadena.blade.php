a9dd16e7b3d6e3ce5274275df628df949e2ad9ba
ferchop89 <fechopacheco@gmail.com>
2018-09-06 11:27:02 -0500

    Version con Informacion por enviar

--- resources/views/menus/sendingCadena.blade.php
+++ resources/views/menus/sendingCadena.blade.php
@@ -24,12 +24,14 @@
                <label for="">Cadena Enviada:</label>
                {{$datos}}
             {{-- </pre> --}}
-            <label for="">CURP:</label>
+            <br /><label for="">CURP:</label>
             {{$curp}}
+            <br /><label for="">URL:</label>
+            {{$URL}}
             <form name="firma" id="firma" target="frameFEA" action="https://enigma.unam.mx/componentefirma/initSigningProcess" method="POST">
-               <input type="hidden" name="datos" value="{{ $datos }}">
-               <input type="hidden" name="URL"      value="{{ $URL }}">
-               <input type="hidden" name="curp" value="{{ $curp }}">
+               <input type="hidden" name="datos" value="||1.0|3|MUOC810214HCHRCR00|Director de Articulación de Procesos|SECRETARÍA DE EDUCACIÓN|Departamento de Control Escolar|23DPR0749T|005|23|UIES180831HDFSEP01|EDGAR|SORIANO|SANCHEZ|2|7.8|2017-01-01T12:05:00||">
+               <input type="hidden" name="URL" value="https://condoc.dgae.unam.mx/registroTitulos/request/firma">
+               <input type="hidden" name="curp" value="UIES180831HDFSEP01">
                <button type="submit" id="btnFirma" class="btn btn-primary">Firmar</button>
             </form>
           </div>
