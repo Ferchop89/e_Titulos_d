2e19baddba8b5e2b2f161855295736ade18e488f
ferchop89 <fechopacheco@gmail.com>
2018-09-06 11:17:12 -0500

    Version con Informacion por enviar

--- app/Http/Controllers/SelloController.php
+++ app/Http/Controllers/SelloController.php
@@ -10,7 +10,8 @@ class SelloController extends Controller
    {
       $URL = "https://condoc.dgae.unam.mx/registroTitulos/request/firma";
       $datos = "||1.0|3|MUOC810214HCHRCR00|Director de Articulación de Procesos|SECRETARÍA DE EDUCACIÓN|Departamento de Control Escolar|23DPR0749T|005|23|SOSE810201HDFRND05|EDGAR|SORIANO|SANCHEZ|2|7.8|2017-01-01T12:05:00||";
-      $curp = "SOSE810201HDFRND05";
+      // $curp = "SOSE810201HDFRND05";
+      $curp = "UIES180831HDFSEP01";
       return view('/menus/sendingCadena', compact('URL', 'datos', 'curp'));
    }
 }
